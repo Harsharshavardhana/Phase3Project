cf3a689a7b46dc540bbed77d0875705097303b18
Khagarajachar <Harshavardhana.Khagarajachar@astronics.com>
2023-07-24 14:47:46 +0530

    Phase3 project

---
+++ src/test/java/simplilearn/com/Phase3Project/API_Test.java
@@ -0,0 +1,49 @@
+package simplilearn.com.Phase3Project;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+public class API_Test extends ReusableMethods{
+
+	@Test
+	public static void main(String[] args)  { 
+		// TODO Auto-generated method stub
+		
+		Response response = getEmployee();
+		// validating ResponseCode as 200
+		Assert.assertEquals(response.statusCode(), 200);
+		
+		response = createNewEmployee("John", "Dae", "100000", "johnDae@gmail.com");
+		// validating ResponseCode as 200
+		Assert.assertEquals(response.statusCode(), 201);
+		// Validating the name in the response is John
+		
+		response = updateEmployee("8");
+		Assert.assertEquals(response.statusCode(), 200);
+		Assert.assertEquals(employeeName, "Tom");
+		Assert.assertNotEquals(response.jsonPath().getString("firstName"), "John");
+
+
+		response = getEmployee("8");
+		Assert.assertEquals(response.statusCode(), 200);
+		Assert.assertEquals(employeeName, "Tom");
+	
+		response = deleteEmployee("8");
+		Assert.assertEquals(response.statusCode(), 200);
+		response=getEmployee("8");
+		Assert.assertNotEquals(response.jsonPath().getString("firstName"), "Tom");
+
+		response=getEmployee("8");
+		Assert.assertEquals(response.statusCode(), 404);
+		
+		
+		response = getEmployee();
+		Assert.assertEquals(response.statusCode(), 200);
+		
+
+	}
+	
+
+}
